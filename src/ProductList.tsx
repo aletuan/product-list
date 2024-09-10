06542a06760a0d1654a0433b832f0045e26723cd
Tuan Anh Le <anhlt1983@gmail.com>
2024-09-10 20:03:51 +0700

    feat: convert project to typescript

---
+++ src/ProductList.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+interface Product {
+  id: number;
+  name: string;
+  price: string;
+}
+
+const ProductList: React.FC = () => {
+  const products: Product[] = [
+    { id: 1, name: 'Product 1', price: '$10' },
+    { id: 2, name: 'Product 2', price: '$20' },
+    { id: 3, name: 'Product 3', price: '$30' },
+  ];
+
+  return (
+    <div>
+      <h2>Product List</h2>
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            {product.name} - {product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductList;

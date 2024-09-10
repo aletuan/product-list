cb02b2b816815342ae838d5cea2d00d2d8e3abc1
Tuan Anh Le <anhlt1983@gmail.com>
2024-09-10 20:22:07 +0700

    Refactor: Update import path for ProductList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import ProductList from './ProductList';
+import ProductList from './components/ProductList';
 
 const App: React.FC = () => {
   return (
